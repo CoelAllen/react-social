67e472e2d274fed08e1754cacc0d915006306e56
Coel Allen <coelallen@gmail.com>
2022-12-09 16:47:49 -0700

    not posting

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
+const User = require("../models/User.js");
 
-router.get("/", (req, res) => {
-  res.send("auth Route");
+router.get("/register", (req, res) => {
+  res.send("Hello from register");
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    const user = await newUser.save();
+    // res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
