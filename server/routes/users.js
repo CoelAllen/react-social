8b7ebbd9391216cd721fbe9e7bf883943cd28129
Coel Allen <coelallen@gmail.com>
2022-12-15 18:21:36 -0700

    get by id and delete by id

--- server/routes/users.js
+++ server/routes/users.js
@@ -24,5 +24,25 @@ router.put("/:id", async (req, res) => {
     return res.status(403).json("You cannot update this account");
   }
 });
-
+router.delete("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (req.body.userId === req.params.id || user?.isAdmin) {
+    try {
+      await User.findByIdAndDelete(user?.id);
+      res.status(200).json("Account has been deleted");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can only delete your own account!");
+  }
+});
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
