d9e433a3bae6e545bb61b1e246e20bc51e35e4a0
Coel Allen <coelallen@gmail.com>
2022-12-15 18:01:42 -0700

    managing user login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,38 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
-  const user = await new User({
-    username: "john",
-    email: "john@email.com",
-    password: "123456",
-  });
-  await user.save();
-  res.send("ok");
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send("User not found");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user?.password
+    );
+    !validPassword && res.status(400).json("Incorrect Password");
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
