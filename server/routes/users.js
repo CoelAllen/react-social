d34c37f65254e14f9e999c68177e2b10887bfe98
Coel Allen <coelallen@gmail.com>
2022-12-21 16:38:12 -0700

    adding following/follower

--- server/routes/users.js
+++ server/routes/users.js
@@ -49,9 +49,32 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    res.status(200).json(user);
+    const { password, updatedAt, ...other } = user?._doc;
+    res.status(200).json(other);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.id);
+      if (!user?.followers.includes(req.body.userId)) {
+        await user?.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser?.updateOne({
+          $push: { followings: req.body.userId },
+        });
+        res.status(200).json("You are now following");
+      } else {
+        res.status(403).json("You are already a follower");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You cannot follow yourself");
   }
 });
 module.exports = router;
